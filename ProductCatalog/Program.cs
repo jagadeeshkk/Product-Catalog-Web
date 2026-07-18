784b2a1598263789a2b55c8146901fb2572bbbb3
jagadeesh k <k.jagadeesh18@gmail.com>
2026-07-18 18:46:04 -0400

    shared message implemented

--- ProductCatalog/Program.cs
+++ ProductCatalog/Program.cs
@@ -26,10 +26,8 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler();
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-//app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
